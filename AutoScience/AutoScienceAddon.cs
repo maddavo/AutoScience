2dd47314da6c33a6f73c2cd226409a60a0e19f05
. <ef314159@vt.edu>
2026-05-17 21:20:17 -0400

    Initial commit
    I'm doing science and I'm still alive~

---
+++ AutoScience/AutoScienceAddon.cs
@@ -0,0 +1,124 @@
+﻿using KSP.UI.Screens;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace AutoScience {
+    [KSPAddon(KSPAddon.Startup.AllGameScenes, false)]
+    class AutoScienceAddon : MonoBehaviour {
+
+        // Settings/GUI stuff
+        private static Texture2D ToolbarIconTexture = null;
+        private static ApplicationLauncherButton ToolbarIcon = null;
+        private static PopupDialog DialogWindow = null;
+        private static readonly string SettingsPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/AutoScience.cfg";
+
+        // Mod settings saved in cfg alongside dll (KSPAddon can't put them in savefile - change?)
+        public static bool ModActive = true;
+        public static bool TrackUnloadedVessels = true;
+        public static bool CollectZeroValueScience = false;
+        public static bool CollectDuplicateScience = false;
+
+        /// <summary>
+        /// Tell vessel module to rebuild its info (experiments list, container, etc) whenever craft is modified
+        /// (Docking, Decoupling, Crashing, EVA construction etc)
+        /// <summary>
+        public void Start() {
+            if (HighLogic.LoadedSceneIsFlight || HighLogic.LoadedScene == GameScenes.SPACECENTER || HighLogic.LoadedScene == GameScenes.TRACKSTATION) {
+                // Load settings from cfg
+                Load();
+                
+                // Rebuild vessel info whenever it's modified (Docking, Decoupling, Crashing, EVA construction etc)
+                GameEvents.onVesselWasModified.Add(vessel => vessel.FindVesselModuleImplementing<AutoScienceVesselModule>().Rebuild());
+
+                // set up toolbar icon and behaviour
+                ToolbarIconTexture = GameDatabase.Instance.GetTexture("Auto Science/Icons/AutoScience", false);
+                ToolbarIcon = ApplicationLauncher.Instance.AddModApplication(ToggleGUI, ToggleGUI, null, null, null, null,
+                    ApplicationLauncher.AppScenes.ALWAYS, ToolbarIconTexture);
+            }
+        }
+
+        public void OnDisable() {
+            ApplicationLauncher.Instance.RemoveModApplication(ToolbarIcon);
+        }
+
+        /// <summary>
+        /// Toggle settings window when user clicks toolbar icon
+        /// </summary>
+        private void ToggleGUI() {
+            if (DialogWindow != null && DialogWindow.enabled) {
+                DialogWindow.Dismiss();
+            } else {
+                DialogWindow = GetOptionsWindow();
+            }
+        }
+
+        /// <summary>
+        /// Serialize mod settings to cfg and save alongside dll
+        /// </summary>
+        private void Save() {
+            ConfigNode.CreateConfigFromObject(this, new ConfigNode(GetType().Name)).Save(SettingsPath);
+            ConfigNode SettingsSave = new ConfigNode();
+            SettingsSave.AddValue("ModActive", ModActive.ToString());
+            SettingsSave.AddValue("TrackUnloadedVessels", TrackUnloadedVessels.ToString());
+            SettingsSave.AddValue("CollectZeroValueScience", CollectZeroValueScience.ToString());
+            SettingsSave.AddValue("CollectDuplicateScience", CollectDuplicateScience.ToString());
+            SettingsSave.Save(SettingsPath);
+        }
+
+        /// <summary>
+        /// Load mod settings from cfg alongside dll
+        /// </summary>
+        private void Load() {
+            if (File.Exists(SettingsPath)) {
+                ConfigNode SettingsSave = ConfigNode.Load(SettingsPath);
+                ModActive = bool.Parse(SettingsSave.GetValue("ModActive"));
+                TrackUnloadedVessels = bool.Parse(SettingsSave.GetValue("TrackUnloadedVessels"));
+                CollectZeroValueScience = bool.Parse(SettingsSave.GetValue("CollectZeroValueScience"));
+                CollectDuplicateScience = bool.Parse(SettingsSave.GetValue("CollectDuplicateScience"));
+            }
+        }
+
+        /// <summary>
+        /// Creates options window each time user clicks toolbar icon
+        /// </summary>
+        private PopupDialog GetOptionsWindow() {
+            DialogGUIVerticalLayout Content = new DialogGUIVerticalLayout(true);
+
+            Content.AddChild(new DialogGUIToggleButton(
+                ModActive,
+                "Mod Active",
+                (Value) => { ModActive = Value; Save(); },
+                -1, 30));
+
+            Content.AddChild(new DialogGUIToggleButton(
+                TrackUnloadedVessels,
+                "Track Unloaded Vessels",
+                (Value) => { TrackUnloadedVessels = Value; Save(); },
+                -1, 30));
+
+            Content.AddChild(new DialogGUIToggleButton(
+                CollectZeroValueScience,
+                "Collect Zero Value Science",
+                (Value) => { CollectZeroValueScience = Value; Save(); },
+                -1, 30));
+
+            Content.AddChild(new DialogGUIToggleButton(
+                CollectDuplicateScience,
+                "Collect Duplicate Science",
+                (Value) => { CollectDuplicateScience = Value; Save(); },
+                -1, 30));
+
+            return PopupDialog.SpawnPopupDialog(
+                new Vector2(0.5f, 0.5f),
+                new Vector2(0.5f, 0.5f),
+                new MultiOptionDialog("AutoScience", "", "AutoScience", HighLogic.UISkin,
+                    new Rect(0.5f, 0.5f, 250f, 100f),
+                    Content),
+                false,
+                HighLogic.UISkin,
+                false
+                );
+        }
+    }
+}
---
+++ AutoScience/AutoScienceVesselModule.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace AutoScience {
+
+    /// Represents a particular unique situation in which science can be run
+    public class LocationData {
+        public ExperimentSituations State;
+        public CelestialBody Body;
+        public String Biome;
+        public bool OnLadder;
+
+        // override Equals and GetHashCode for storing in HashSet
+
+        public override bool Equals(object obj) {
+            if (obj == null) return false;
+            LocationData other = obj as LocationData;
+            if (other != null) return State == other.State && Body == other.Body && Biome == other.Biome && OnLadder == other.OnLadder;
+            else return false;
+        }
+
+        public override int GetHashCode() {
+            return State.GetHashCode() ^ Body.GetHashCode() ^ Biome.GetHashCode() ^ OnLadder.GetHashCode();
+        }
+    }
+
+    /// The meat of the plugin, runs science automatically on any loaded vessel and queues location data on unloaded vessels
+    class AutoScienceVesselModule : VesselModule {
+
+        public LocationData Location;
+
+        // Cache list of runnable experiments so we're not iterating over potentially-massive vessels every time
+        public List<ModuleScienceExperiment> Experiments;
+
+        // Cache the first science container we find (usually the root command pod) because same
+        public List<ModuleScienceContainer> Containers;
+
+        // Scientist onboard for resettable experiments? (goo etc)
+        public bool HasScientist;
+
+        // Queue visited biomes while vessel is unloaded, to run science the next time it's loaded
+        // Use HashSet to enforce uniqueness and O(1) insert
+        HashSet<LocationData> QueuedBiomes = new HashSet<LocationData>();
+
+        // Does this vessel have a location and potentially QueuedBiomes? (generate on first update frame)
+        public bool initialized = false;
+
+        // Is this vessel loaded (Experiments, Container info is valid)?
+        public bool loaded = false;
+
+        // Check for science again after rebuilding (might have added parts when docking, EVA construction)
+        public bool needsUpdate;
+
+        // Hack - don't do science immediately after loading (EVA Kerbals are not immediately attached to the ladder)
+        public float spawnTime;
+
+        /// <summary>
+        /// Check if we've moved locations and need to run science again
+        /// Also updates our location so that GetBiome() is only called once (expensive operation? involves texture lookup)
+        /// Runs per vessel per frame!!
+        /// <summary>
+        private bool CheckMove() {
+            String NewBiome = GetBiome();
+
+            bool moved = (
+                Location.State != ScienceUtil.GetExperimentSituation(Vessel.EVALadderVessel) ||
+                Location.Body != Vessel.mainBody ||
+                Location.Biome != NewBiome ||
+                Location.OnLadder != (Vessel.EVALadderVessel != Vessel));
+
+            if (moved) {
+                Location.State = ScienceUtil.GetExperimentSituation(Vessel.EVALadderVessel);
+                Location.Body = Vessel.mainBody;
+                Location.Biome = NewBiome;
+                Location.OnLadder = Vessel.EVALadderVessel != Vessel;
+            }
+
+            return moved;
+        }
+        
+        /// <summary>
+        /// First run of FixedUpdate(), make sure data is good
+        /// </summary>
+        public void InitVessel() {
+            spawnTime = UnityEngine.Time.time;
+            Location = new LocationData();
+            CheckMove();
+            Rebuild();
+            initialized = true;
+        }
+
+        /// <summary>
+        /// Vessel was modified, is newly loaded, etc - do expensive per-part operations here and stash the results
+        /// </summary>
+        public void Rebuild() {
+            Experiments = Vessel.FindPartModulesImplementing<ModuleScienceExperiment>();
+            Containers = Vessel.FindPartModulesImplementing<ModuleScienceContainer>();
+            loaded = true;
+            needsUpdate = true;
+        }
+
+        // All info for this module is regenerated when vessel loads - the only thing that needs to
+        // persist across scenes/saves is the queued locations for unloaded vessels.
+
+        /// <summary>
+        /// Save any queued biomes when switching scenes/saving game
+        /// </summary>
+        protected override void OnSave(ConfigNode node) {
+            node.ClearNodes();
+
+            if (!AutoScienceAddon.TrackUnloadedVessels) return;
+
+            foreach (LocationData d in QueuedBiomes) {
+                ConfigNode newNode = new ConfigNode("StoredLocation");
+                newNode.AddValue("State", (int)d.State);
+                newNode.AddValue("Body", d.Body.bodyName);
+                newNode.AddValue("Biome", d.Biome);
+                newNode.AddValue("OnLadder", d.OnLadder.ToString());
+                node.AddNode(newNode);
+            }
+        }
+
+        /// <summary>
+        /// Load any queued biomes when switching scenes/loading game
+        /// </summary>
+        protected override void OnLoad(ConfigNode node) {
+            foreach (ConfigNode storedNode in node.GetNodes("StoredLocation")) {
+                LocationData newLocation = new LocationData();
+                newLocation.State = (ExperimentSituations)Int32.Parse(storedNode.GetValue("State"));
+                newLocation.Body = FlightGlobals.Bodies.FirstOrDefault(b => b.bodyName == storedNode.GetValue("Body"));
+                newLocation.Biome = storedNode.GetValue("Biome");
+                newLocation.OnLadder = bool.Parse(storedNode.GetValue("OnLadder"));
+                QueuedBiomes.Add(newLocation);
+            }
+        }
+        
+        /// <summary>
+        /// Check for new situations to run science every game tic
+        /// </summary>
+        public void FixedUpdate() {
+            if (!AutoScienceAddon.ModActive) return;
+
+            // ensure location data is valid on first run
+            if (!initialized) InitVessel();
+
+            // don't run science immediately after vessel init, wait a bit
+            if (spawnTime < UnityEngine.Time.time - 0.2f) {
+
+                // only try to run science on loaded vessels that we have full part data for
+                if (Vessel.loaded) {
+
+                    // call Rebuild() if it hasn't been
+                    if (!loaded) Rebuild();
+
+                    // check for queued data
+                    if (AutoScienceAddon.TrackUnloadedVessels && QueuedBiomes.Any()) {
+                        foreach (LocationData d in QueuedBiomes) {
+                            CheckScience(d.State, d.Body, d.Biome);
+                        }
+                        QueuedBiomes.Clear();
+                    }
+
+                    // I'm doing science and I'm still alive~
+                    if ((CheckMove() || needsUpdate) && Containers.Any()) {
+                        needsUpdate = false;
+                        CheckScience(ScienceUtil.GetExperimentSituation(Vessel), Vessel.mainBody, GetBiome());
+                    }
+                } else if (AutoScienceAddon.TrackUnloadedVessels) {
+                    if (loaded) loaded = false;
+                    if (CheckMove()) {
+                        QueueLocation();
+                    }
+                }
+            } 
+        }
+        
+        /// <summary>
+        /// Vessel is unloaded, queue the current situation to check for science later
+        /// </summary>
+        private void QueueLocation() {
+            LocationData NewLocation = new LocationData();
+            NewLocation.State = Location.State;
+            NewLocation.Body = Location.Body;
+            NewLocation.Biome = Location.Biome;
+            NewLocation.OnLadder = Location.OnLadder;
+
+            QueuedBiomes.Add(NewLocation);
+        }
+        
+        /// <summary>
+        /// Check for any new science to do!
+        /// Must inject all relevant location data rather than using the vessel's current state
+        /// (We might be running queued science from a vessel that was unloaded)
+        /// </summary>
+        public void CheckScience(ExperimentSituations s, CelestialBody b, String Biome) {
+            HasScientist = Vessel.GetVesselCrew().Any(k => k.trait == KerbalRoster.scientistTrait);
+            foreach (ModuleScienceExperiment e in Experiments) {
+                TryScience(e, s, b, Biome);
+            }
+        }
+
+        /// <summary>
+        /// Only do surface samples if they're available
+        /// Copied from ForScience! without checking if it's 100% correct
+        /// </summary>
+        public bool SurfaceSamplesUnlocked() {
+            return GameVariables.Instance.UnlockedEVA(ScenarioUpgradeableFacilities.GetFacilityLevel(SpaceCenterFacility.AstronautComplex))
+                && GameVariables.Instance.UnlockedFuelTransfer(ScenarioUpgradeableFacilities.GetFacilityLevel(SpaceCenterFacility.ResearchAndDevelopment));
+        }
+        
+        /// <summary>
+        /// Run a particular experiment in a given situation if possible
+        /// </summary>
+        public void TryScience(ModuleScienceExperiment e, ExperimentSituations s, CelestialBody b, String Biome) {
+            if (!e || !e.vessel || !Containers.Any()) return; // this should never fail but better safe than sorry
+
+            // Don't cheat the goo/materials bay experiments
+            if (!e.rerunnable && !HasScientist) return;
+
+            // Don't run an experiment that isn't available (duh)
+            if (!e.experiment.IsAvailableWhile(s, b)) return;
+
+            // Don't run an EVA experiment unless you actually have one
+            // (Per-part iteration across vessel, but only if requiresInventoryPart is true; in stock this should only ever be an EVA Kerbal?)
+            if (e.requiresInventoryPart) {
+                bool found = false;
+
+                // issues with Vessel.protoVessel.GetAllProtoPartsIncludingCargo(), using this instead
+                foreach(ModuleInventoryPart i in Vessel.FindPartModulesImplementing<ModuleInventoryPart>()) {
+                    if (i.storedParts.Values.Any(p => p.partName == e.requiredInventoryPart)) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) return;
+            }
+
+            if (!SurfaceSamplesUnlocked() && e.experiment.id == "surfaceSample") return;
+
+            // Don't run science while on a ladder when your craft is landed
+            // DO run science when splashed down, since it's frustrating to try and get back up on the pod sometimes :)
+            if (Vessel.EVALadderVessel != Vessel && s == ExperimentSituations.SrfLanded) return;
+
+            // Generate science data
+            ScienceSubject subject = GetScienceSubject(e.experiment, s, b, Biome);
+            ScienceData data = new ScienceData(e.experiment.baseValue * subject.dataScale, e.xmitDataScalar, 0f, subject.id, subject.title);
+
+            // Don't do zero-value science (TODO: toolbar option)
+            if (!AutoScienceAddon.CollectZeroValueScience && ResearchAndDevelopment.GetScienceValue(e.experiment.baseValue * e.experiment.dataScale, subject) < 0.1) return;
+
+            // Finally!
+            if (AutoScienceAddon.CollectDuplicateScience) {
+                foreach(ModuleScienceContainer c in Containers) {
+                    if (!c.HasData(data)) c.AddData(data);
+                }
+            } else {
+                if (!HasData(data)) Containers.First().AddData(data);
+            }
+        }
+
+        /// <summary>
+        /// Checks whether this vessel contains the given data in any of its science containers
+        /// </summary>
+        public bool HasData(ScienceData data) {
+            if (Containers.Any(c => c.HasData(data))) return true;
+
+            // Also check parent vessel if we're a Kerbal on a ladder
+            if (Vessel.EVALadderVessel != Vessel) {
+                AutoScienceVesselModule ParentModule = Vessel.EVALadderVessel.FindVesselModuleImplementing<AutoScienceVesselModule>();
+                return ParentModule.Containers.Any(c => c.HasData(data));
+            }
+
+            return false;
+        }
+        
+        /// <summary>
+        /// Get current biome, including all the KSC buildings ("LandedAt" if it exists)
+        /// </summary>
+        public String GetBiome() {
+            if (Vessel.mainBody.BiomeMap == null) return null;
+            if (!string.IsNullOrEmpty(Vessel.landedAt)) return Vessel.GetLandedAtString(Vessel.landedAt);
+            else return ScienceUtil.GetExperimentBiome(Vessel.mainBody, Vessel.latitude, Vessel.longitude);
+        }
+
+        private ScienceSubject GetScienceSubject(ScienceExperiment e, ExperimentSituations s, CelestialBody b, String Biome) {
+            return ResearchAndDevelopment.GetExperimentSubject(
+                    e,
+                    ScienceUtil.GetExperimentSituation(Vessel),
+                    b,
+                    e.BiomeIsRelevantWhile(s) ? Biome : String.Empty,
+                    null);
+        }
+    }
+}
